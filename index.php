becba0ca87f90d3b69d9fd641a545ffa83aa2f38
Shokry <shokry92@gmail.com>
2016-06-08 04:05:12 +0200

    generate the name at the client side

--- index.php
+++ index.php
@@ -3,11 +3,37 @@ include 'NameManager.php';
 
 $nameManager = new NameManager();
 
-echo $nameManager->getCodename();
 ?>
 
-<form method="post" action="CallManager.php">
-	<input type="text" name="adjective" placeholder="adjective">
-	<input type="text" name="name" placeholder="name">
-	<input type="submit" value="Send">
-</form>
+<html>
+    <head></head>
+    <body>
+        <h3 id="codename"> <?= $nameManager->getCodename(); ?> </h3>
+        <button type="button" onclick="getCodename()"> Next, please </button>
+        <form method="post" action="CallManager.php">
+            <input type="text" name="adjective" placeholder="adjective">
+            <input type="text" name="name" placeholder="name">
+            <button type="button" value="Send">
+        </form>
+
+        <script type="text/javascript">
+            names = <?= json_encode($nameManager->getData('names')); ?>;
+            adjectives = <?= json_encode($nameManager->getData('adjectives')); ?>;
+
+            function getCodename() {
+            
+              console.log(names);
+            }
+
+            function getCodename() {
+                //Generate some random integer
+                //to be used as an index to the array holding the names/adjectives
+                var randomName = names[Math.floor((Math.random() * names.length))];
+                var randomAdjective = adjectives[Math.floor((Math.random() * adjectives.length))];
+
+                document.querySelector("#codename").textContent = randomAdjective + ' ' + randomName;
+                
+            }
+        </script>
+    </body>
+</html>
